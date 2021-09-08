88fb1aaa8aba7b2ec9ce297650d66a147c7c9fdb
Matthew <mhillis57@gmail.com>
2021-09-08 02:14:08 -0500

    added get methods to get student records

--- studentserver.js
+++ studentserver.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');//web server uses this to manage files on a local computer
+const glob = require("glob");
 
 
 app.use(bodyParser.json());
@@ -36,6 +37,54 @@ app.post('/students', function(req,res){
         }
     });//end of writeFile method
 });//end of post method
+
+app.get('/students/:record_id', function(req,res){
+    var record_id = req.params.record_id;
+
+    fs.readFile("students/" + record_id + ".json", "utf8", function(err,data){
+        if(err) {
+            var rsp_obj = {};
+            rsp_obj.record_id = record_id;
+            rsp_obj.message - 'error - resource not found';
+            return res.status(404).send(rsp_obj);
+        }else{
+            return res.status(200).send(data);
+        }
+    });
+});
+//start of read all files section
+function readFiles(files, arr, res){
+    fname = files.pop();
+    if(!fname)
+    return;
+    fs.readFile(fname, "utf8", function(err,data){
+        if(err){
+            return res.status(500).send({"message":"error - internal server error"});
+        } else{
+            arr.push(JSON.parse(data));
+            if(files.length == 0) {
+                var obj = {};
+                obj.students = arr;
+                return res.status(200).send(obj);
+            }else{
+                readFiles(files,arr,res);
+            }
+        }
+    });
+}
+
+app.get('/students', function(req,res){
+    var obj = {};
+    var arr = [];
+    filesread = 0;
+
+    glob("students/*.json",null,function ( err, files){
+        if(err){
+            return res.status(500).send({"message":"error - internal server error"});
+        }
+            readFiles(files,[],res);
+    });
+});
 
 app.listen(5678);//start server 
 console.log('Server is running...');
